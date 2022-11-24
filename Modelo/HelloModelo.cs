587b7bc09ca1bca17c6a6c2899387fcaf6994eea
Gabriela Bravo Morinishi <gabrielabravo.usp@gmail.com>
2022-11-24 13:48:41 -0300

    Adicionar arquivos de projeto.

---
+++ Modelo/HelloModelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapHelloWord.Modelo
+{
+    internal class HelloModelo
+    {
+        public string Mensagem = "Olá FiapON";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using FiapHelloWord.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapHelloWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloModelo modelo = new HelloModelo();
+
+            Console.WriteLine(modelo.Mensagem);
+            Console.Read();
+        }
+    }
+}
