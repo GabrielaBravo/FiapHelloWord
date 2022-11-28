13b90ff23ce903e4098d8d487cf3202dc1dac593
Gabriela Bravo Morinishi <gabrielabravo.usp@gmail.com>
2022-11-28 10:48:26 -0300

    aula 2

---
+++ Modelo/ClasseBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapHelloWord.Modelo
+{
+    internal class ClasseBase
+    {
+    }
+}
